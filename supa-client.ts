20f9dc08db34829c93930f18766accb4cd25e5fb
heehortus <hee.hortus@gmail.com>
2026-05-17 03:35:30 +0900

    update supabase

--- supa-client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const makeBrowserClient = () => {
-  const supabaseUrl = (import.meta as any).env.VITE_SUPABASE_URL;
-  const supabaseKey = (import.meta as any).env.VITE_SUPABASE_ANON_KEY;
-  const client = createClient(supabaseUrl, supabaseKey);
-  return client;
-};
